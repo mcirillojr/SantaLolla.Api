6ffb842d1918c5697613105c247fcdde336944df
Marcio Cirillo <mcirillojr@gmail.com>
2026-06-23 15:25:57 -0300

    enviando log.

---
+++ SantaLolla.Api/Models/Logs/ApiLogAcesso.cs
@@ -0,0 +1,21 @@
+﻿namespace SantaLolla.Api.Models.Logs
+{
+    public class ApiLogAcesso
+    {
+        public long? IdTerceiro { get; set; }
+        public string? ClientId { get; set; }
+        public string? NomeTerceiro { get; set; }
+
+        public string MetodoHttp { get; set; } = string.Empty;
+        public string Endpoint { get; set; } = string.Empty;
+        public string? QueryString { get; set; }
+
+        public int? StatusCode { get; set; }
+        public int? TempoRespostaMs { get; set; }
+
+        public string? IpOrigem { get; set; }
+        public string? UserAgent { get; set; }
+
+        public string? MensagemErro { get; set; };
+    }
+}
